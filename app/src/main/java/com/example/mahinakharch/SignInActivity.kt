7dd531edc58735844560a9d674cd1045bfbb4254
Costfy: Budget Tracker <khushichaudhary904@gmail.com>
2023-08-16 19:22:47 +0530

    16-08-2023

--- app/src/main/java/com/example/mahinakharch/SignInActivity.kt
+++ app/src/main/java/com/example/mahinakharch/SignInActivity.kt
@@ -1,11 +1,43 @@
 package com.example.mahinakharch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mahinakharch.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseauth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        firebaseauth = FirebaseAuth.getInstance()
+        setContentView(binding.root)
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                    firebaseauth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+            } else {
+                Toast.makeText(this, "Please enter a valid input!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mahinakharch/SignUpActivity.kt
+++ app/src/main/java/com/example/mahinakharch/SignUpActivity.kt
@@ -1,11 +1,52 @@
 package com.example.mahinakharch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
+import com.example.mahinakharch.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseauth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding= ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseauth=FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener{
+            val intent=Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener{
+        val email=binding.emailEt.text.toString()
+            val pass=binding.passET.text.toString()
+            val confirmpass=binding.confirmPassEt.text.toString()
+         if(email.isNotEmpty()&&pass.isNotEmpty()&&confirmpass.isNotEmpty())   {
+             if(pass.equals(confirmpass)){
+             firebaseauth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener{
+                 if(it.isSuccessful){
+                   val intent=Intent(this,SignInActivity::class.java)
+                     startActivity(intent)
+                 }
+                 else{
+                     Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+
+                 }
+             }
+             }
+             else{
+                 Toast.makeText(this,"Password is not matching",Toast.LENGTH_SHORT).show()
+             }
+         }
+         else{
+             Toast.makeText(this,"Please enter a valid input!",Toast.LENGTH_SHORT).show()
+
+         }
+        }
     }
 }
