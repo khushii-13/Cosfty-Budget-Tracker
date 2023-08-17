3f52a47424da7be866e0e014a43a921172292b7a
Costfy: Budget Tracker <khushichaudhary904@gmail.com>
2023-08-17 15:49:16 +0530

    17-08-2023

--- app/src/main/java/com/example/mahinakharch/SignUpActivity.kt
+++ app/src/main/java/com/example/mahinakharch/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mahinakharch
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySignUpBinding
     private lateinit var firebaseauth:FirebaseAuth
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivitySignUpBinding.inflate(layoutInflater)
